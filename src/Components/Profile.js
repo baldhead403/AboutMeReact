27e0ed6589b46d7f2b4825382c764fd7d1ca000d
COR Students <corstudents@Matts-MBP-97.localdomain>
2019-01-31 09:22:13 -0500

    enter

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,16 +6,19 @@ class Profile extends Component {
         return(
             <div>
                <div class="row" id="profile">
-               <div class="col-sm-6" id="row1">
+               <div class="col-sm-12" id="row1">
                   <div class="col-sm-12">
                     <h1>ABOUT ME</h1>
                 </div> 
                 </div>
           
           <div class="col-sm-4">
-              <div class="col-sm-12">
+              <div class="col-sm-10">
           <p>
-              
+          Experienced Junior Software Engineer with a demonstrated history of working in the non-profit organization management industry. 
+          Skilled in Microsoft Client , Microsoft Server Administrator, Analytical Skills, Mobile Application Development, Web Application Development, Server Administration, 
+          Cisco Network Administrator, PC and Network Technician  and Accounting. 
+          Strong engineering professional with a Associate of Science (A.S.) focused in System, Networking, and LAN/WAN Management/Manager and Computer from Atlanta Technical College.    
          </p>
               </div>
           </div>

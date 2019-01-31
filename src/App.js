a871c8e5ed767379ed2ba7817595080c1e5bc500
COR Students <corstudents@Matts-MBP-97.localdomain>
2019-01-31 08:21:03 -0500

    enter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import profile_pic from './Images/profile_pic.jpg'
+import Title from './Components/Title';
+// import Profile from './Components/Profile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="20180524_095010.jpg" alt="graduation"/>
-          <img src="" alt="" />
-          
+        <div>
+          <Title/>
+          {/* <img src={require()}/> */}
+        </div>  
         </header>
       </div>
     );
   }
 }
+// class Info extends Component{
+//   render(){
+//     return(
+//       <body>
+
+//       <div>
+//         <Title/>
+//         {/* <img src={require()}/> */}
+//       </div>
+//       </body>
+//     )
+//   }
+// }
+
 
 export default App;
---
+++ src/Components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Donald Rivers</h1>
+                <p>Full Stack Web Developer In Transition</p>
+                 
+            </div>
+        )
+    }
+}
+
+export default Title
